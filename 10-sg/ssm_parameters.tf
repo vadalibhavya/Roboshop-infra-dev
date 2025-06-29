d00a3aca64f0091d650510419deb33c52b6b4e1d
aditya <adityavittal92@gmail.com>
2025-06-29 22:31:35 +0530

    instance create and update

--- 10-sg/ssm_parameters.tf
+++ 10-sg/ssm_parameters.tf
@@ -4,3 +4,10 @@ resource "aws_ssm_parameter" "frontend_sg_id" {
   type        = "String"
   value       = module.frontend.sg_id
 }
+
+resource "aws_ssm_parameter" "bastion_sg_id" {
+  name        = "/${var.project}/bastion_sg_id"
+  description = "Security group id for bastion"
+  type        = "String"
+  value       = module.bastion.sg_id
+}
