b589381ab2bb1db463d3c056d41385798fa2372c
aditya <adityavittal92@gmail.com>
2025-06-29 22:48:01 +0530

    instance create and update

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -2,5 +2,5 @@
 resource "aws_instance" "bastion" {
   ami              = local.ami_id
   instance_type    = var.instance_type
-  vpc_security_group_ids = [local.bastion_sg_id]
+  vpc_security_group_ids = local.bastion_sg_id
 }
